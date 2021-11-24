1a871cdfbb53085868a978f728fc65acda09e462
Alan7829 <alanalexismartinez@gmail.com>
2021-11-24 17:00:05 -0600

    Add last settings of login

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:brawlteca/src/app.dart';
+import 'package:brawlteca/tools/shared_preferences.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  final prefs = PreferenciasUsuario.instance;
   runApp(MyApp());
+  await Firebase.initializeApp();
+  await prefs.initPrefs();
 }
